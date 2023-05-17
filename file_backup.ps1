b28f07fb9a602998b6413bd5de8669e74bab5811
Frollamma <fral01@protonmail.com>
2023-05-17 09:33:27 +0200

    Improve file backup

--- file_backup.ps1
+++ file_backup.ps1
@@ -11,17 +11,20 @@ $drive = "C"
 $directories = @(
 	"scanner",
 	#	"Users\" + $user + "\Desktop\",
-	"Users\" + $user + "\Documents\",
-	"Users\" + $user + "\Download\"
+	"Users\$user\Documents\",
+	"Users\$user\Download\"
 )
 
 foreach ($dir in $directories) {
 	$path = "\\$ip\$drive$\$dir"
+	$backupPath = "C:\scanner"
 
-	# Write-Host "Copying " + $dir
-	# copy /y ("\\" + $ip + "\c$\" + $dir + "\*") $dir
+	# Write-Host "Copying " + $path
+	# copy /y ("\\" + $ip + "\c$\" + $path + "\*") $path
 	Write-Host "Zipping $path"
 	$zipDir = $path + ".rar"
 	$command = "& `'$winrarPath`' a `'$zipDir`' `'$path`'"
 	Start-Process -FilePath "powershell.exe" -ArgumentList $command -Wait
+	
+	Write-Host "Copying $path to $backupPath"
 }
